0a949dee8e0cb7eef924bb5b7c14a1e40ac9a26b
EL Abquina <eabquina@gmail.com>
2026-02-23 22:22:38 +0800

    remove reference modules

--- working_schedule_adv/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import resource_calendar
-from . import resource_calendar_change
-from . import resource_calendar_change_lines
-from . import resource_calendar_change_type
--- working_schedule_adv/models/resource_calendar_change_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, models, fields
-
-class ResourceCalendarChangeType(models.Model):
-    _name = 'resource.calendar.change.type'
-    _description = 'Work Schedule Change Type'
-
-    name = fields.Char('Name', required=True)
-    code = fields.Char('Code', required=True)
