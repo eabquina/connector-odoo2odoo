84e1634b1fd189cecf24b11b86e44036ab3d8f60
Aldrei Santua <main.aldreijustin.santua@cvsu.edu.ph>
2026-02-28 19:51:00 +0800

    [FIX] backend sync: dedupe batch jobs with identity key

--- connector_odoo/models/odoo_backend/common.py
+++ connector_odoo/models/odoo_backend/common.py
@@ -462,7 +462,12 @@ class OdooBackend(models.Model):
                 )
             else:
                 from_date = None
-            self.env[model].with_delay().import_batch(backend, filters)
+            identity_key = (
+                f"odoo.import_batch:{model}:backend:{backend.id}:{from_date_field}"
+            )
+            self.env[model].with_delay(identity_key=identity_key).import_batch(
+                backend, filters
+            )
 
         next_time = import_start_time - timedelta(seconds=IMPORT_DELTA_BUFFER)
         self.write({from_date_field: next_time})
@@ -502,6 +507,11 @@ class OdooBackend(models.Model):
                 filters.append(("write_date", ">", from_date))
             else:
                 from_date = None
-            self.env[model].with_delay().export_batch(backend, filters)
+            identity_key = (
+                f"odoo.export_batch:{model}:backend:{backend.id}:{from_date_field}"
+            )
+            self.env[model].with_delay(identity_key=identity_key).export_batch(
+                backend, filters
+            )
         next_time = import_start_time - timedelta(seconds=IMPORT_DELTA_BUFFER)
         self.write({from_date_field: next_time})
