06e4ce0a6f4e86e36230305ce2bfde4d6e9b70c5
EL Abquina <eabquina@gmail.com>
2026-04-08 10:00:13 +0800

    [FIX] connector_odoo: hide sale and picking queue fields for non-managers

--- connector_odoo/models/sale_order/common.py
+++ connector_odoo/models/sale_order/common.py
@@ -134,6 +134,7 @@ class SaleOrder(models.Model):
 
     queue_job_ids = fields.Many2many(
         comodel_name="queue.job",
+        groups="queue_job.group_queue_job_manager",
     )
 
     def action_confirm(self):
